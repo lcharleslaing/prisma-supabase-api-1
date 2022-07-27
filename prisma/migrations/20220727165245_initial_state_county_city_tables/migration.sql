cd31d36c4875d77518f4107aa5dea65b8f124604
leecharleslaing <71520336+lcharleslaing@users.noreply.github.com>
2022-07-27 12:57:19 -0400

    Updated to supabase DB

--- prisma/migrations/20220727165245_initial_state_county_city_tables/migration.sql
+++ prisma/migrations/20220727165245_initial_state_county_city_tables/migration.sql
@@ -1,27 +1,4 @@
 -- CreateTable
-CREATE TABLE "Product" (
-    "id" SERIAL NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
-    "name" TEXT NOT NULL,
-    "qty" INTEGER NOT NULL DEFAULT 1,
-    "price" INTEGER NOT NULL DEFAULT 999,
-    "categoryId" INTEGER,
-
-    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Category" (
-    "id" SERIAL NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
-    "name" TEXT NOT NULL,
-
-    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "State" (
     "id" SERIAL NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -55,22 +32,7 @@ CREATE TABLE "City" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
-
--- CreateIndex
 CREATE UNIQUE INDEX "State_name_key" ON "State"("name");
-
--- CreateIndex
-CREATE UNIQUE INDEX "County_name_key" ON "County"("name");
-
--- CreateIndex
-CREATE UNIQUE INDEX "City_name_key" ON "City"("name");
-
--- AddForeignKey
-ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "County" ADD CONSTRAINT "County_stateId_fkey" FOREIGN KEY ("stateId") REFERENCES "State"("id") ON DELETE SET NULL ON UPDATE CASCADE;
